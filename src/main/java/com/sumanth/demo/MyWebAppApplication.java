f2b110564dea62c93facc111a8625ab02056a2c9
Challa Sumanth <challasumanth44@gmail.com>
2023-05-27 18:31:05 +0530

    Accepting client data and passing to jsp

---
+++ src/main/java/com/sumanth/demo/MyWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sumanth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppApplication.class, args);
+	}
+
+}
